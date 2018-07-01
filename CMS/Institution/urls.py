590b74e3ec5811137a42d0c55c922ca011a97743
only-someone <490072639@qq.com>
2018-07-01 10:12:56 +0800

    url

--- CMS/Institution/urls.py
+++ CMS/Institution/urls.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
 from django.conf.urls import url, include
-from .views import InstitutionViewSet
+from .views import InstitutionViewSet,EmployeeViewSet
 
 router = routers.DefaultRouter()
 router.register('Institution', InstitutionViewSet)
+router.register('Employee', EmployeeViewSet)
 urlpatterns = router.urls 
