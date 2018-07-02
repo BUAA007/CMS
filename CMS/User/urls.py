81eafd12779f9ead391e5f353560b4fd659f6560
marco <1007284166@qq.com>
2018-07-02 13:56:55 +0800

    syw1.0

--- CMS/User/urls.py
+++ CMS/User/urls.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from django.conf.urls import url, include
 from User.views import *
 router = routers.DefaultRouter()
-router.register('', UserViewSet)
+router.register('user', UserViewSet)
 router.register('join',JoinViewSet)
 
 urlpatterns = router.urls
