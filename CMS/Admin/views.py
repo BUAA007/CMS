60ae636103e7e0c3cc254f69a941339b27a0be06
ypq96 <540670802@qq.com>
2018-07-06 09:29:57 +0800

    test admin登入

--- CMS/Admin/views.py
+++ CMS/Admin/views.py
@@ -19,9 +19,9 @@ class AdminViewSet(viewsets.ModelViewSet):
 		#user_id=request.session['id']
 		institution_id=request.data.get('id')
 		thisinstitution=Institution.objects.get(id=institution_id)
-		thisinstitution.status=1
+		thisinstitution.status='1'
 		thisinstitution.save()
-		return Response( "成功 ",status = status.HTTP_200_OK)
+		return Response( {"info":"成功 "},status = status.HTTP_200_OK)
 
 	'''
 	def retrive(self,request,pk=None):#显示某一未通过审核的机构的法人信息？渲染到那个网页？institution/intitution/2/
@@ -37,13 +37,34 @@ class AdminViewSet(viewsets.ModelViewSet):
 		#NEED MODIFY
 	'''
 	@action(methods=['GET'], detail=False)
-	def allinstitution(self, request):  # 显示所有未通过审核机构的基本信息
+	def adminCMS(self, request):  # 显示所有未通过审核机构的基本信息
 		allinstitution = Institution.objects.filter(status="0")
 		allinstitutionse = InstitutionSerializer(allinstitution, many=True)
 		return Response(allinstitutionse.data,status=status.HTTP_200_OK)
 
-		template = loader.get_template('conference.html')
+		template = loader.get_template('admin_list.html')
 		context = {
 			'institution': allinstitution,
 		}
 		return HttpResponse(template.render(context, request))
+
+	@action(methods=['POST'], detail=False)
+	def login(self, request):
+		if request.method == "POST":
+			username = request.data.get('username')
+			password = request.data.get('password')
+			try:
+				admin = Admin.objects.get(username=username, password=password)
+				if admin:
+					request.session['is_login'] = 'true'  # 定义session信息
+					request.session['username'] = username
+					request.session['id'] = admin.id
+					request.session['type'] = '2'  # 普通用户标记
+					request.session.set_expiry(0)
+					# template = loader.get_template('base.html')
+					# context = {}
+					return HttpResponse(info('登陆成功'), content_type="application/json")
+			except:
+				pass
+			# return render(request,'base.html',status = status.HTTP_201_CREATED)                ## 登录成功就将url重定向到后台的url
+		return HttpResponse(errorInfo('用户名或密码不正确'), content_type="application/json")
