42142456fabcb6e82e3ed28ac44b35e9a7c66a7b
only-someone <490072639@qq.com>
2018-07-07 15:36:25 +0800

    注册聆听

--- CMS/Institution/views.py
+++ CMS/Institution/views.py
@@ -238,19 +238,19 @@ class EmployeeViewSet(viewsets.ModelViewSet):
             sys.path.append('../')
             email=[thispaper.owner.email]
             cmsem.send_mail(email, "论文审核结果",
-                            "您的论文已经被审核完成,您的论文需要修改后重新投稿。请登录http://www.cmsys.tk/user/user/allpaper/")
+                            "您的论文已经被审核完成,您的论文需要修改后重新投稿。请登录http://www.cmsys.tk/")
         elif thisstatus == "1":
             thispaper.status = thisstatus
             thispaper.save()
             sys.path.append('../')
             email = [thispaper.owner.email]
-            cmsem.send_mail(email, "论文审核结果", "您的论文已经被审核完成.您的论文已经通过。请登录http://www.cmsys.tk/user/user/allpaper/")
+            cmsem.send_mail(email, "论文审核结果", "您的论文已经被审核完成.您的论文已经通过。请登录http://www.cmsys.tk/")
         elif thisstatus == "-2":
             thispaper.status = thisstatus
             thispaper.save()
             sys.path.append('../')
             email = [thispaper.owner.email]
-            cmsem.send_mail(email, "论文审核结果", "您的论文已经被审核完成.不好意思，您的论文被拒绝。请登录http://www.cmsys.tk/user/user/allpaper/")
+            cmsem.send_mail(email, "论文审核结果", "您的论文已经被审核完成.不好意思，您的论文被拒绝。请登录http://www.cmsys.tk/")
         return Response("成功 ", status=status.HTTP_200_OK)
 
    @action(methods=['GET'], detail=False)
