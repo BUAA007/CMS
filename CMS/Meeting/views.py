8778b391322943ed5cb9a52c7ebcff9708a0fe71
h529987 <572260394@qq.com>
2018-07-08 13:20:37 +0800

    excel

--- CMS/Meeting/views.py
+++ CMS/Meeting/views.py
@@ -945,7 +945,7 @@ class MeetingViewSet(viewsets.ModelViewSet):
 		response = StreamingHttpResponse(file_iterator(url))
 		response['Content-Type'] = 'application/vnd.ms-excel'
 		from urllib import parse
-		excel_name = str(thismeeting.title) + "joininfo"
+		excel_name = str(thismeeting.meeting_id) + "joininfo"
 		response['Content-Disposition'] = 'attachment;filename=' + parse.quote(excel_name) + '.xls'
 		return response
 
